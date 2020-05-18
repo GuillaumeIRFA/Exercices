df2db4d3f99989e833d55ecdbe08c7e24bc7bced
GuillaumeIRFA <64774032+GuillaumeIRFA@users.noreply.github.com>
2020-05-18 15:48:04 +0200

    deeded

--- EXO javascript/nombrePremier.js
+++ EXO javascript/nombrePremier.js
@@ -1,14 +1,23 @@
-let diviseur, premier;
-for (let nombre = 1; nombre <= 100; nombre++) {
-    diviseur = 1, premier = 0;
-    do {
-        if (nombre % diviseur == 0) {
-            premier++;
-        }
-        diviseur++;
-    } while (diviseur <= nombre)
+// for (let nombre = 2, diviseur, premier; nombre <= 100; nombre++) {
+//     diviseur = 2, premier = 0;
+//     while (diviseur <= nombre) {
+//         if (nombre % diviseur == 0) {
+//             premier++;
+//         }
+//         diviseur++;
+//     }
 
-    if (premier <= 2) {
+//     if (premier <= 1) {
+//         document.write(nombre + " est un nombre premier!<br>");
+//     }
+// }
+
+let nombre, diviseur, premier;
+for (nombre = 2; nombre <= 100; nombre++) {
+    for (diviseur = 2, premier = 0; diviseur <= nombre; diviseur++) {
+        if (nombre % diviseur == 0) { premier++; }
+    }
+    if (premier <= 1) {
         document.write(nombre + " est un nombre premier!<br>");
     }
 }
