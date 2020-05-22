8e50bd7d60d4e643f34e7de87d594a51a20285f0
GuillaumeIRFA <64774032+GuillaumeIRFA@users.noreply.github.com>
2020-05-22 17:13:27 +0200

    Create exempleClasses.js

---
+++ EXO Javascript/exempleClasses.js
@@ -0,0 +1,23 @@
+class Personnage {
+    constructor(arme, armure) {
+        this.arme = new Arme(arme);
+        this.armure = new Armure(armure);
+    }
+
+}
+
+class Arme {
+    constructor(arme) {
+        this.armeType = arme;
+        this.dmg = 50;
+        this.atkspd = 2;
+    }
+}
+
+class Armure {
+    constructor(armure) {
+        this.armureType = armure;
+        this.def = 20;
+        this.dura = 100;
+    }
+}
