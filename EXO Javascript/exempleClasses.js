7f326fc3e1b0f6db660f8d58af25978d118cefde
GuillaumeIRFA <64774032+GuillaumeIRFA@users.noreply.github.com>
2020-05-27 11:40:15 +0200

    casse brique

--- EXO Javascript/exempleClasses.js
+++ EXO Javascript/exempleClasses.js
@@ -3,6 +3,8 @@ class Personnage {
         this.arme = new Arme(arme);
         this.armure = new Armure(armure);
     }
+
+
 }
 
 class Arme {
@@ -11,10 +13,20 @@ class Arme {
         this.dmg = 50;
         this.atkspd = 2;
     }
+
+    Youplaboom() {
+        this.dmg = 80;
+        return 'xd';
+    }
+
+    ChangeArme(arme) {
+        this.armeType = arme;
+    }
 }
 
 class Armure {
     constructor(armure) {
+        if (!armure) return;
         this.armureType = armure;
         this.def = 20;
         this.dura = 100;
