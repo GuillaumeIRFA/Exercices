00e97f31909c273fb9df3b051f2b3f27359da205
GuillaumeIRFA <64774032+GuillaumeIRFA@users.noreply.github.com>
2020-06-11 17:33:56 +0200

    exo QCM

---
+++ EXO Javascript/12-QCM/qcm.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    $(".question").css({ background: "orange", border: "1px solid black", clear: "both", width: "100%", height: "250px", marginBottom: "30px", display: "flex" });
+    $(".reponse").addClass("hidden");
+    $("img").css({ alignSelf: "center", justifySelf: "flex-end", marginLeft: "auto" });
+
+    const ReponsesIndex = [0, 0, 1];
+
+    $("a").hover(
+        function() {
+            $(".question").each(function(index) {
+                currentSelector = $(`[name="q${index+1}"]`);
+                if (currentSelector[ReponsesIndex[index]].checked == true) {
+                    $(`#reponse${index+1}`).css({ color: "green" });
+                    $(`#img${index+1}`).attr("src", "./bon.png");
+                } else {
+                    $(`#reponse${index+1}`).css({ color: "red" });
+                    $(`#img${index+1}`).attr("src", "./mauvais.png");
+                }
+
+            });
+            $(".reponse").toggleClass("hidden");
+        },
+        function() {
+            $("img").attr("src", "./question.png");
+            $(".reponse").toggleClass("hidden");
+        }
+    )
+
+});
