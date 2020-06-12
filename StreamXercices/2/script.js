105aecfbf1279eed669e04d5e61e19b344be8d0b
GuillaumeIRFA <64774032+GuillaumeIRFA@users.noreply.github.com>
2020-06-12 21:11:57 +0200

    Update script.js

--- StreamXercices/2/script.js
+++ StreamXercices/2/script.js
@@ -7,12 +7,10 @@ let itemSet = new Set();
 itemSet.add("Sceau").add("Pierre");
 
 while (true) {
-
     let item = prompt("A quel objet pensez vous?");
     let itemQuantity = parseInt(prompt("Quelle est la quantité de cet objet?"));
 
     if (item && itemQuantity) {
-
         if (!itemSet.has(item)) {
             itemSet.add(item);
             itemsTable[item] = itemQuantity;
@@ -20,13 +18,8 @@ while (true) {
             itemsTable[item] += itemQuantity;
         }
 
-        let message = ''
-        Object.keys(itemsTable).forEach(function(key) {
-            message += `${key} : ${itemsTable[key]} unité(es) \n`
-        });
-
+        let message = '';
+        Object.entries(itemsTable).forEach(([key, value]) => message += `${key} : ${value} unité(es) \n`);
         alert(message);
-        console.log(itemSet, itemsTable);
-
     } else break;
 }
