94207ae6f8e3d716e794b0b5feb332ce5edf6fac
GuillaumeIRFA <64774032+GuillaumeIRFA@users.noreply.github.com>
2020-06-12 19:57:18 +0200

    Update script.js

--- StreamXercices/2/script.js
+++ StreamXercices/2/script.js
@@ -1,20 +1,23 @@
 let itemsTable = {
     "Sceau": 5,
     "Pierre": 2,
-}
+};
+
 
 while (true) {
     let item = prompt("A quel objet pensez vous?");
     let itemQuantity = parseInt(prompt("Quelle est la quantité de cet objet?"));
-    if (item in itemsTable == false) {
-        itemsTable[item] = itemQuantity;
-    } else {
-        itemsTable[item] += itemQuantity;
+    if (item && itemQuantity) {
+        if (item in itemsTable == false) {
+            itemsTable[item] = itemQuantity;
+        } else {
+            itemsTable[item] += itemQuantity;
+        }
     }
 
     let message = ''
     for (key in itemsTable) {
-        message += `${key} : ${itemsTable[key]}u \n`
+        message += `${key} : ${itemsTable[key]} unité(es) \n`
     }
     alert(message);
 }
