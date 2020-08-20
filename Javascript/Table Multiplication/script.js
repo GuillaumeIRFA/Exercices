a9e062fad168d238b0947620bf9e48a7a3fe805f
GuillaumeIRFA <64774032+GuillaumeIRFA@users.noreply.github.com>
2020-08-20 09:49:22 +0200

    table multiplication
    initial commit

---
+++ Javascript/Table Multiplication/script.js
@@ -0,0 +1,12 @@
+let nombre, tableau = [];
+
+// while (true) {
+nombre = parseInt(prompt("Veuillez entrer un nombre : "));
+for (i = 1; i <= 10; i++) {
+    tableau[i - 1] = nombre * i;
+}
+
+for (i = 0; i < tableau.length; i++) {
+    document.write(`${i+1} * ${nombre} = ${tableau[i]}<br>`);
+}
+// }
